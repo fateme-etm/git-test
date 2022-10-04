32e6d2d7c6fb3ae61e6935146b291e3624eb866b
fateme <fatemeetminan98@gmail.com>
2022-10-04 18:42:07 +0330

    services component added

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const toggler = document.querySelector(".menu");
-const navbar = document.querySelector(".navbar");
+// const toggler = document.querySelector(".menu");
+// const navbar = document.querySelector(".navbar");
 
-toggler.addEventListener("click",()=>{
-    navbar.classList.toggle("navbar-expanded");
+// toggler.addEventListener("click",()=>{
+//     navbar.classList.toggle("navbar-expanded");
+// });
+
+const tabs = document.querySelectorAll(".services__tab");
+const tabDetails = document.querySelectorAll(".services__detail");
+
+
+tabs.forEach(tab=>{
+    tab.addEventListener("click", (e)=>{
+        const tabTargetContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+        console.log(tab.dataset.tabTarget)
+        console.log(tabTargetContent);
+        tabs.forEach(tab=>tab.classList.remove("active"));
+        tabDetails.forEach(tabDetail=>tabDetail.classList.remove("active"));
+
+        tab.classList.add("active");
+        tabTargetContent.classList.add("active");
+
+    });
+    
 });
